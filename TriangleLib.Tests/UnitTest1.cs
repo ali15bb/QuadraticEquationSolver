560c9007642a77f8ba29d6b1f5daf86d9f45aa92
ali <agnaniali@gmail.com>
2025-02-06 14:30:00 +0100

    Final project with triangle function and unit tests

---
+++ TriangleLib.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using TriangleLib;
+
+public class TriangleTests
+{
+    [Theory]
+    [InlineData(3, 4, 5, true)]  // Valid triangle
+    [InlineData(1, 1, 3, false)] // Invalid triangle
+    [InlineData(5, 5, 5, true)]  // Equilateral triangle
+    [InlineData(0, 0, 0, false)] // Zero-length sides
+    public void TestIsTriangle(int a, int b, int c, bool expected)
+    {
+        Assert.Equal(expected, TriangleChecker.IsTriangle(a, b, c));
+    }
+}
---
+++ TriangleLib/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace TriangleLib
+{
+    public class TriangleChecker
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            return (a + b > c) && (a + c > b) && (b + c > a);
+        }
+    }
+}
